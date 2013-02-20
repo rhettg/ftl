be9b097ffc871afad7530e51c78349a38a4b41f8
Rhett Garber <rhettg@gmail.com>
2013-02-19 16:16:01 -0800

    Starting a refactor for a better structured package repository

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import "io"
 import "time"
 import "encoding/binary"
 import "encoding/base64"
+//import "path"
 import "path/filepath"
 import "crypto/md5"
 import "strings"
@@ -20,6 +21,49 @@ func optFail(message string) {
 		fmt.Println(message)
 		fmt.Print(goopt.Help())
 		os.Exit(1)
+}
+
+type PackageRepository struct {
+	Name string
+	BasePath string
+}
+
+func (pr *PackageRepository) List() (localRevisions []string) {
+	packagePath := filepath.Join(pr.BasePath, pr.Name, "revs")
+	
+	localRevisions = make([]string, 1000)[0:0]
+	
+	packageFile, err := os.Open(packagePath)
+	if err != nil {
+		fmt.Println("Failed to open", packagePath)
+		return
+	}
+
+	localRevisionFiles, err := packageFile.Readdir(1024)
+	if err != nil {
+		if err.Error() == "EOF" {
+			// Nothing
+		} else {
+			fmt.Println("Failed to package file", packageFile, err)
+			return
+		}
+	}
+	
+	for _, fileInfo := range localRevisionFiles {
+		localRevisions = append(localRevisions, fileInfo.Name())
+	}
+	return
+}
+
+func (pr *PackageRepository) Add(name string, r io.Reader) (err error)  {
+	_ = name
+	_ = r
+	return
+}
+
+func (pr *PackageRepository) Remove(name string) (err error)  {
+	_ = name
+	return 
 }
 
 func encodeBytes(b []byte) (s string) {
@@ -58,15 +102,15 @@ func buildRevisionId(fileName string) (string, error) {
 }
 
 func blessedRevision(bucket *s3.Bucket, packageName string) string {
-	blessFile := fmt.Sprintf("%s.bless", packageName)
+	revFile := fmt.Sprintf("%s.rev", packageName)
 	
-	data, err := bucket.Get(blessFile)
+	data, err := bucket.Get(revFile)
 	if err != nil {
 		s3Error, _ := err.(*s3.Error)
 		if s3Error.StatusCode == 404 {
 			return ""
 		} else {
-			fmt.Printf("Error finding bless file", err)
+			fmt.Printf("Error finding rev file", err)
 			return ""
 		}
 	}
@@ -107,37 +151,155 @@ func spoolCmd(bucket *s3.Bucket, fileName string) {
 
 }
 
-func syncCmd() {
-	fmt.Println("Sync")
-}
-
-func blessCmd(bucket *s3.Bucket, blessName string) {
-	blessParts := strings.Split(blessName, ".")
-	packageName := blessParts[0]
-	revisionName := blessParts[1]
+func downloadPackageRevision(bucket *s3.Bucket, pkg *PackageRepository, revisionName string) {
+	fmt.Println("Download", revisionName)
 	
-	blessFile := fmt.Sprintf("%s.bless", packageName)
-	
-	err := bucket.Put(blessFile, []byte(revisionName), "text/plain", s3.Private)
+	listResp, err := bucket.List(revisionName + ".", ".", "", 1000)
 	if err != nil {
-		fmt.Printf("Failed to put bless file", err)
+		fmt.Println("Failed listing", err)
+		return 
+	}
+	
+	//fmt.Println(listResp)
+	
+	for _, prefix := range listResp.Contents {
+		fmt.Println("Found", prefix.Key)
+		r, err := bucket.GetReader(prefix.Key)
+		if err != nil {
+			fmt.Println("Error opening", prefix.Key, err)
+			continue
 		}
+		defer r.Close()
+		
+		pkg.Add(prefix.Key, r)
+
+		/*
+		w, err := os.Create(packagePath + "/" + prefix.Key)
+		if err != nil {
+			fmt.Println("Failed to create", packagePath + "/" + prefix.Key, err)
+			continue
+		}
+		defer w.Close()
+		
+		_, err = io.Copy(w, r)
+		if err != nil {
+			fmt.Println("Failed to copy", err)
+			continue
+		}
+		*/
+	}
 
 }
 
-func listCmd(bucket *s3.Bucket, packageName string) {
-	blessed := blessedRevision(bucket, packageName)
+func removePackageRevision(pkg *PackageRepository, revisionName string) {
+	fmt.Println("Remove", revisionName)
+	_ = pkg.Remove(revisionName)
+}
+
+func syncPackage(bucket *s3.Bucket, pkg *PackageRepository) {
+	fmt.Println("Syncing", pkg.Name, "to path", pkg.BasePath)
 	
+	remoteRevisions := listPackageRevisions(bucket, pkg.Name)
+	localRevisions := pkg.List()
+	
+	fmt.Println("Found", len(remoteRevisions), "remote and", len(localRevisions), "local")
+	
+	remoteNdx, localNdx := 0, 0
+	for done := false; !done; {
+		if remoteNdx < len(remoteRevisions) {
+			fmt.Println("Remote", remoteRevisions[remoteNdx])
+		}
+		if localNdx < len(localRevisions) {
+			fmt.Println("Local", localRevisions[localNdx])
+		}
+		
+		switch {
+		case remoteNdx >= len(remoteRevisions) && localNdx >= len(localRevisions):
+			done = true
+		case remoteNdx >= len(remoteRevisions):
+			// We have local revisions, than remote... hmm
+			done = true
+		case localNdx >= len(localRevisions):
+			// We have more remote revisions than local, just download what's left
+			downloadPackageRevision(bucket, pkg, remoteRevisions[remoteNdx])
+			remoteNdx++
+		case remoteRevisions[remoteNdx] > localRevisions[localNdx]:
+			// We have an extra local revision, remove it
+			removePackageRevision(pkg, localRevisions[localNdx])
+			localNdx++
+		case remoteRevisions[remoteNdx] < localRevisions[localNdx]:
+			// We have a new remote revision, download it
+			downloadPackageRevision(bucket, pkg, remoteRevisions[remoteNdx])
+			remoteNdx++
+		case remoteRevisions[remoteNdx] == localRevisions[localNdx]:
+			remoteNdx++
+			localNdx++
+		}
+	}
+	
+}
+
+func syncCmd(bucket *s3.Bucket, ftlRoot string) {
+	rootFile, err := os.Open(ftlRoot)
+	if err != nil {
+		fmt.Println("Failed to open root", ftlRoot, err)
+		return
+	}
+
+	dirContents, err := rootFile.Readdir(1024)
+	if err != nil {
+		if err.Error() == "EOF" {
+			// Nothing
+		} else {
+			fmt.Println("Failed to read root", ftlRoot, err)
+			return
+		}
+	}
+	
+	for _, file := range dirContents {
+		if file.IsDir() {
+			pkg := PackageRepository{ftlRoot, file.Name()}
+			syncPackage(bucket, &pkg)
+		}
+	}
+	
+}
+
+func jumpCmd(bucket *s3.Bucket, revName string) {
+	revParts := strings.Split(revName, ".")
+	packageName := revParts[0]
+	revision := revParts[1]
+	
+	revFile := fmt.Sprintf("%s.rev", packageName)
+	
+	err := bucket.Put(revFile, []byte(revision), "text/plain", s3.Private)
+	if err != nil {
+		fmt.Printf("Failed to put rev file", err)
+	}
+}
+
+func listPackageRevisions(bucket *s3.Bucket, packageName string) (revisionList []string) {
+	revisionList = make([]string, 1000)[0:0]
 	listResp, err := bucket.List(packageName + ".", ".", "", 1000)
 	if err != nil {
 		fmt.Println("Failed listing", err)
-		return
+		return 
 	}
 	
 	for _, prefix := range listResp.CommonPrefixes {
 		revisionName := prefix[:len(prefix)-1]
-		if strings.HasSuffix(revisionName, blessed) {
-			fmt.Printf("%s\t(blessed)\n", revisionName)
+		revisionList = append(revisionList, revisionName)
+	}
+	
+	return
+}
+
+func listCmd(bucket *s3.Bucket, packageName string) {
+	activeRev := blessedRevision(bucket, packageName)
+	
+	for _, revisionName := range listPackageRevisions(bucket, packageName) {
+		if len(activeRev) > 0 && strings.HasSuffix(revisionName, activeRev) {
+			fmt.Printf("%s\t(active)\n", revisionName)
 		} else {
 			fmt.Println(revisionName)
 		}
@@ -164,6 +326,15 @@ func main() {
 	goopt.Summary = "Deploy system built around S3."
 	goopt.Parse(nil)
 	
+	ftlRootEnv := os.Getenv("FTL_ROOT")
+	if len(ftlRootEnv) == 0 {
+		optFail(fmt.Sprintf("FTL_ROOT not set"))
+	}		
+	ftlRoot, err := filepath.Abs(ftlRootEnv)
+	if err != nil {
+		optFail("Invalid FTL_ROOT")
+	}
+	
 	auth, err := aws.EnvAuth()
     if err != nil {
 		optFail(fmt.Sprintf("AWS error: %s", err))
@@ -188,12 +359,12 @@ func main() {
 				} else {
 					optFail("Missing file name")
 				}
-			case "bless":
+			case "jump":
 				if (len(goopt.Args) > 1) {
-					blessName := strings.TrimSpace(goopt.Args[1])
-					blessCmd(bucket, blessName)
+					revName := strings.TrimSpace(goopt.Args[1])
+					jumpCmd(bucket, revName)
 				} else {
-					optFail("Bless what?")
+					optFail("Jump where?")
 				}
 			case "list":
 				if (len(goopt.Args) > 1) {
@@ -202,7 +373,7 @@ func main() {
 						listPackagesCmd(bucket)
 					}
 			case "sync":
-				syncCmd()
+				syncCmd(bucket, ftlRoot)
 			default:
 				optFail(fmt.Sprintf("Invalid command: %s", cmd))
 		}
